d7fdecb58ab7ea69e5511f2a7967bcc99b7792c3
defauser99 <defaihsanr@gmail.com>
2021-01-16 12:51:16 +0700

    Initial commit

---
+++ app/src/main/java/binar/ch4/mylinkedinlogin/MainActivity.kt
@@ -0,0 +1,11 @@
+package binar.ch4.mylinkedinlogin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
